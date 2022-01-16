b736ce352f984a99e8bf130b84fb5b9afcb3d91f
saurabhburade <sburade3@gmail.com>
2022-01-16 18:43:12 +0530

    Feat : add field update methods

--- contracts/UserContract.sol
+++ contracts/UserContract.sol
@@ -7,6 +7,12 @@ contract User {
     address admin;
     string bio = "default";
     string profileImage;
+    string coverImage;
+    struct Blog {
+        address _address;
+        uint256 timestamp;
+        uint256 createdBlock;
+    }
 
     constructor(
         string memory _userName,
@@ -16,6 +22,23 @@ contract User {
         userName = _userName;
         profileImage = _profileImage;
         owner = _owner;
+    }
+
+    // Function to add/update bio
+    function updateBio(string memory _bio) public {
+        bio = _bio;
+    }
+
+    // Function to add/update cover image
+
+    function updateCoverImage(string memory _cover) public {
+        coverImage = _cover;
+    }
+
+    // Function to update profile image
+
+    function updateProfileImage(string memory _profileImg) public {
+        profileImage = _profileImg;
     }
 
     function getFields()
